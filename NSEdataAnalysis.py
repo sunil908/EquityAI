39595c2c274a41d5cd15cee7a197efc58dc9494a
Zorro30 <gbp301195@gmail.com>
2019-05-28 00:27:27 +0530

    Added function to find data for all 1600 scripts

--- NSEdataAnalysis.py
+++ NSEdataAnalysis.py
@@ -12,6 +12,7 @@ import talib
 from nsepy.history import get_price_list
 from sqlalchemy import create_engine
 import matplotlib
+import csv
 matplotlib.use('PS')
 
 #ta-lib library for analysis
@@ -63,32 +64,53 @@ def setNSEDailyQuoteColList():
 
 def applyADX(nsedailyquoteDF):
     
-    adxdf = pd.Series(talib.ADX(nsedailyquoteDF['HIGH'].values, nsedailyquoteDF['LOW'].values, nsedailyquoteDF['CLOSE'].values, timeperiod = 14), index = nsedailyquoteDF.TIMESTAMP, name = 'ADX_%s' % str(14))
-    print(adxdf)
-    adxdf.to_csv('adx.csv',index=True) 
+    adxdf = pd.Series(talib.ADX(nsedailyquoteDF['HIGH'].values, nsedailyquoteDF['LOW'].values, nsedailyquoteDF['CLOSE'].values, timeperiod = 14), index = nsedailyquoteDF.TIMESTAMP, name = 'ADX')
+    # print(adxdf)
+    # adxdf.to_csv('adx.csv',index=True)
+    return adxdf 
 
 def applyADXR(nsedailyquoteDF):
 
-    adxrdf = pd.Series(talib.ADXR(nsedailyquoteDF['HIGH'].values, nsedailyquoteDF['LOW'].values, nsedailyquoteDF['CLOSE'].values, timeperiod = 14), index = nsedailyquoteDF.TIMESTAMP, name = 'ADX_%s' % str(14))
+    adxrdf = pd.Series(talib.ADXR(nsedailyquoteDF['HIGH'].values, nsedailyquoteDF['LOW'].values, nsedailyquoteDF['CLOSE'].values, timeperiod = 14), index = nsedailyquoteDF.TIMESTAMP, name = 'ADXR')
     return adxrdf
 
 def applyAPO(nsedailyquoteDF):
 
-    apodf = pd.Series(talib.APO(nsedailyquoteDF['CLOSE'].values, fastperiod=12, slowperiod=26, matype=0), index = nsedailyquoteDF.TIMESTAMP, name = 'ADX_%s' % str(14))
+    apodf = pd.Series(talib.APO(nsedailyquoteDF['CLOSE'].values, fastperiod=12, slowperiod=26, matype=0), index = nsedailyquoteDF.TIMESTAMP, name = 'APO')
     return apodf
 
 def applyAROONOSC(nsedailyquoteDF):
 
-    arronoscdf = pd.Series(talib.AROONOSC(nsedailyquoteDF['HIGH'].values, nsedailyquoteDF['LOW'].values, timeperiod = 14), index = nsedailyquoteDF.TIMESTAMP, name = 'ADX_%s' % str(14))
+    arronoscdf = pd.Series(talib.AROONOSC(nsedailyquoteDF['HIGH'].values, nsedailyquoteDF['LOW'].values, timeperiod = 14), index = nsedailyquoteDF.TIMESTAMP, name = 'AROONOSC')
     return arronoscdf
 
 def applyCCI(nsedailyquoteDF):
 
-    ccidf = pd.Series(talib.CCI(nsedailyquoteDF['HIGH'].values, nsedailyquoteDF['LOW'].values, nsedailyquoteDF['CLOSE'].values, timeperiod = 14), index = nsedailyquoteDF.TIMESTAMP, name = 'ADX_%s' % str(14))
+    ccidf = pd.Series(talib.CCI(nsedailyquoteDF['HIGH'].values, nsedailyquoteDF['LOW'].values, nsedailyquoteDF['CLOSE'].values, timeperiod = 14), index = nsedailyquoteDF.TIMESTAMP, name = 'CCI')
     return ccidf
 
 # getNSEDailyQuote(start_date,end_date,filename)
-nsedailyquoteDF = readNSEDailyQuote(filename,'RELIANCE',find_start_date=script_start_date,find_end_date=script_end_date)
+# nsedailyquoteDF = readNSEDailyQuote(filename,'RELIANCE',find_start_date=script_start_date,find_end_date=script_end_date)
 # print(nsedailyquoteDF)
-print(applyADX(nsedailyquoteDF))
+# print(applyADX(nsedailyquoteDF))
 # readNSEDailyQuote(filename,'SBIN',find_start_date=script_start_date,find_end_date=script_end_date)
+
+
+# apply adx to all the scripts and store in ADX/ folder.
+def getADXForAll(filename):
+
+    data = pd.read_csv('{}.csv'.format(filename),names= getNSEDailyQuoteColList(),dtype=setNSEDailyQuoteColList(),usecols=list(setNSEDailyQuoteColList().keys()))
+
+    scripts = data['SYMBOL'].unique()
+    for i in scripts:
+        per_data = data[(data['SYMBOL'] == i) & (data['SERIES']== 'EQ')]
+        if len(per_data) != 0: 
+            applied_adx = applyADX(per_data)
+            applied_adxr = applyADXR(per_data)
+            applied_apo = applyAPO(per_data)
+            applied_aroonosc = applyAROONOSC(per_data)
+            applied_cci  = applyCCI(per_data)
+            df = pd.concat([applied_adx, applied_adxr, applied_apo, applied_aroonosc, applied_cci], axis=1)
+            df.to_csv('data/{}.csv'.format(i))
+
+# getADXForAll('output')
